fb30c694ea2ce55644592a0b222b413a5b6466c5
Renat <renat@MacBook-Air-Renat.local>
2024-06-16 18:53:06 +0300

    Простое решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+using System.ComponentModel.DataAnnotations;
+
+string[] array = { "Hello", "2", "world", ":-)" };
+
+int len = 0;
+foreach (var item in array)
+{
+    if (item.Length <= 3)
+    {
+        len++;
+
+    }
+}
+string[] arrayThreeSymbols = new string[len];
+int correntIndex = 0;
+foreach (var item in array)
+{
+    if (item.Length <= 3)
+    {
+        arrayThreeSymbols[correntIndex] = item;
+        correntIndex++;
+    }
+}
+Console.Write("[");
+for (int i = 0; i < arrayThreeSymbols.Length; i++)
+{
+    if (i == arrayThreeSymbols.Length - 1)
+    {
+        Console.Write($"\"{arrayThreeSymbols[i]}\"");
+    }
+    else
+    {
+        Console.Write($"\"{arrayThreeSymbols[i]}\", ");
+    }
+}
+Console.Write("]");
+
+///
+
+
+
+
+
+
+
+
